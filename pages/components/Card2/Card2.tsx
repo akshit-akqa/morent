c7beaf2c0821aabf54779b993bfc6a4a9da22c60
Akshit Tyagi <akshit.tyagi@akqa.com>
2023-02-22 14:13:43 +0530

    commit13

--- pages/components/Card2/Card2.tsx
+++ pages/components/Card2/Card2.tsx
@@ -33,7 +33,7 @@ const Card2 = (props: ICard1440375Props) => {
          <Name name={name} type={type}/>
          {/* <Image src={plp} className={styles.car} alt="d"/> */}
          {/* <img src="//images.ctfassets.net/curk6g4u6s3f/68ftsvkbejYn1EhtCh3fMU/18727e4f068983a175eb1d919945d18f/koenigsegg.svg" className={styles.car} alt="d"/> */}
-         <Image src={carImage.fields.file.url} width={100} height={100} className={styles.car} alt="img"/>
+         <Image src={carImage.fields?.file.url} width={100} height={100} className={styles.car} alt="img"/>
          
          <Specs mileage={mileage} transmission={transmission} space={space}/>
          <Price price={price}/>
